8ed01634e6996bb8ea346b290cfd263d7f3bfd69
Meeket Ketan Tank <86684823+Meeketank@users.noreply.github.com>
2025-11-12 16:24:49 +0530

    Fix class name from EnhancedBackgroundLearner to BackgroundLearner

--- online_learning/__init__.py
+++ online_learning/__init__.py
@@ -1,7 +1,7 @@
 """Online Learning Module for Continuous Model Improvement"""
 
-from .background_learner import EnhancedBackgroundLearner
+from .background_learner import BackgroundLearner
 from .incremental_trainer import IncrementalTrainer
 from .accuracy_tracker import AccuracyTracker
 
-__all__ = ['EnhancedBackgroundLearner', 'IncrementalTrainer', 'AccuracyTracker']
+__all__ = ['BackgroundLearner', 'IncrementalTrainer', 'AccuracyTracker']
